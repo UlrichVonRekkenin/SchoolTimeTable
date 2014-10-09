c8e4545f6fd5101edfc7518dfd65bc48ff1d2e0c
UlrichVonRekkenin <ulrihlebedev@gmail.com>
2014-10-09 21:02:48 +0400

    Nicer.

--- date.py
+++ date.py
@@ -1,33 +1,22 @@
-## pip install py-dateutil
 # -*- coding: utf-8 -*-
 
 from os import path, mkdir
-from datetime import date as D
-from dateutil.rrule import rrule, DAILY
-from collections import OrderedDict as OD
+from datetime import date
+from datetime import timedelta
+import calendar
 
-'''
-init should be a dict with a struct as:
-   init['year'] a tuple of academic year: (datetime.date(start), datetime.date(finish)) 
-   init['excludes'] a list of tuple pairs: [(datetime.date(exclude_start1), datetime.date(exclude_finish1))]
-       where datetime.date(fmt) with format fmt=%Y, %m, %d
-   init['classes'] a dict of dictionaries with a struct as:
-        init['classes']['class_name1'] is a dict with struct as:
-            {isoweekday1: numberOfLesson1, isoweekday_n: numberOfLesson_n}
-                where isoweekday_n in [1..7]
-'''
 init = {
-        'year': (D(2014, 9, 1), D(2015, 5, 31)),           
+        'year': ((2014, 9, 1), (2015, 5, 31)),           
 
         'excludes': [
-                    (D(2014, 11, 3), D(2014, 11, 9)),
-                    (D(2014, 12, 29), D(2015, 1, 11)),
-                    (D(2015, 2, 23), D(2015, 2, 23)),
-                    (D(2015, 3, 9), D(2015, 3, 9)),
-                    (D(2015, 3, 23), D(2015, 3, 29)),
-                    (D(2015, 5, 1), D(2015, 5, 4)),
-                    (D(2015, 5, 11), D(2015, 5, 11)),
-                    (D(2015, 5, 25), D(2015, 5, 31))
+                    ((2014, 11, 3 ), (2014, 11, 9 )),
+                    ((2014, 12, 29), (2015, 1 , 11)),
+                    ((2015, 2 , 23), (2015, 2 , 23)),
+                    ((2015, 3 , 9 ), (2015, 3 , 9 )),
+                    ((2015, 3 , 23), (2015, 3 , 29)),
+                    ((2015, 5 , 1 ), (2015, 5 , 4 )),
+                    ((2015, 5 , 11), (2015, 5 , 11)),
+                    ((2015, 5 , 25), (2015, 5 , 31))
                     ],
 
         'classes': {
@@ -43,55 +32,48 @@ init = {
                     }
         }
 
-DAYS = {1: 'Mon',
-        2: 'Tue',
-        3: 'Wed',
-        4: 'Thu',
-        5: 'Fri',
-        6: 'Sat',
-        7: 'Sun'}
-
 def GetSchoolTimeTable(init):
 
     out = 'out'
 
-    if 'year' in init and 'excludes' in init and 'classes' in init:
-        
-        for class_name in init['classes'].keys():
-            d = dict()
-            lessCounter = 0
+    for class_name in init['classes'].keys():
+        d = dict()
+        lessCounter = 0
 
-            for dt in rrule(DAILY, dtstart=init['year'][0], until=init['year'][1]):
-                dt = dt.date()
-                _, _, weekday = dt.isocalendar()
+        dt = date(*init['year'][0])
+        while dt != date(*init['year'][1]):
 
-                if weekday in init['classes'][class_name].keys():
+            if dt.isoweekday() in init['classes'][class_name].keys():
 
-                    if any(x[0] <= dt <= x[1] for x in init['excludes']):
-                        lessCounter += 1
+                if any(date(*x[0]) <= dt <= date(*x[1]) for x in init['excludes']):
+                    lessCounter += 1
 
-                    else:
-                        d[dt.strftime('%Y.%m.%d')] = '{}-{}'.format(DAYS[weekday], init['classes'][class_name][weekday]) 
+                else:
+                    d[dt.strftime('%Y.%m.%d')] = '{}-{}'.format(
+                            calendar.day_abbr[dt.isoweekday()],
+                            init['classes'][class_name][dt.isoweekday()]
+                        ) 
 
-            if not path.exists(out):
-                mkdir(out)
+            dt += timedelta(days = 1)
 
-            with open(path.join(out, '{}.txt'.format(class_name)), 'w') as f:
-                import datetime
+        if not path.exists(out):
+            mkdir(out)
 
-                od = OD(sorted(d.items()))
+        with open(path.join(out, '{}.txt'.format(class_name)), 'w') as f:
+            import datetime
 
-                f.write(
-                    'Class: {};\tTotal hours: {};\tLost hours: {}\nExcludes:\n\t{}\n\n{}'.format(
-                        class_name,
-                        len(d.keys()),
-                        lessCounter,
-                        str.join('\n\t', (str.format('{} -> {}', *x) for x in init['excludes'])),
-                        str.join('\n', (
-                            str.format('{}\t{}', v, datetime.datetime.strptime(k, '%Y.%m.%d').strftime('%d.%m.%Y')) for k, v in od.items()
-                            ))
-                    )
+            f.write(
+                'Class: {};\tTotal hours: {};\tLost hours: {}\nExcludes:\n\t{}\n\n{}'.format(
+                    class_name,
+                    len(d.keys()),
+                    lessCounter,
+                    str.join('\n\t', ('{} -> {}'.format(*x) for x in init['excludes'])),
+                    str.join('\n', (
+                        '{}\t{}'.format(v, datetime.datetime.strptime(k, '%Y.%m.%d').strftime('%d.%m.%Y')) for k, v in sorted(d.items())
+                        ))
                 )
+            )
 
 if __name__ == '__main__':
     GetSchoolTimeTable(init)
+
